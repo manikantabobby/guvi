2892356ff17958b16805fee977de15d506eeba5a
manikantabobby <51699475+manikantabobby@users.noreply.github.com>
2019-07-18 09:40:04 +0530

    Update checking vowel or consonant.c

--- checking vowel or consonant.c
+++ checking vowel or consonant.c
@@ -7,7 +7,7 @@ int main()
     {
         printf("Vowel");
     }
-    else if(ch>'a'&&ch<'z'||ch>'A'&&ch<'z')
+    else if(ch>'a'&&ch<'z'||ch>'A'&&ch<'Z')
     {
         printf("Consonant");
     }
